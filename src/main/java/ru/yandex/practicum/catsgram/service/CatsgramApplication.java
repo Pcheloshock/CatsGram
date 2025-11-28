1bd3720a7198979e828e931d9c1fa1fe33b590c7
pcheloshock <kraniys@yandex.ru>
2025-11-28 15:17:29 +0500

    0.0.1.

--- src/main/java/ru/yandex/practicum/catsgram/service/CatsgramApplication.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/CatsgramApplication.java
@@ -2,10 +2,16 @@ package ru.yandex.practicum.catsgram.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
 
 @SpringBootApplication
 public class CatsgramApplication {
     public static void main(String[] args) {
         SpringApplication.run(CatsgramApplication.class, args);
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
